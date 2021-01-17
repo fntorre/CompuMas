16100fbbc80e225c37124ba75152cacd002c8c11
name <email>
2021-01-17 19:47:35 -0300

    nueevos

--- js/compuMasNico.js
+++ js/compuMasNico.js
@@ -20,3 +20,39 @@ window.onload=function()  {
 
 }
 
+function verCompu(){
+    vtexjs.checkout.getOrderForm().then(function (orderForm) {
+        console.log(orderForm);
+        let marketingData = orderForm.marketingData;
+        console.log(marketingData);
+        if (marketingData == null) {
+            console.log("no hay utm");
+        } else {
+            var cond = marketingData.utmSource;
+            if (cond == "compumas") {
+                $("#payment-group-custom202PaymentGroupPaymentGroup").css("display", "block")
+            } else {
+                console.log("no tiene la utm compumas")
+            }
+        }
+        
+
+
+    });
+
+    let cuotas = localStorage.getItem('cuotas');
+    let valor = localStorage.getItem('valor')
+
+    const compuCuotas = document.querySelector(".box-payment-group2.box-payment-option.custom202PaymentGroupPaymentGroup");
+    compuCuotas.textContent = "Usted a elegido abonar su compra con CompuMas en " + cuotas + " de GS " + valor ;
+
+    const div = document.createElement("div");
+    div.textContent = "Su compra con crédito CompuMAS esta siendo procesada por el departamento Financiero. Nos podremos en contacto a la brevedad.";
+
+    const compuCuotasInfo = document.querySelector(".box-payment-group2.box-payment-option.custom202PaymentGroupPaymentGroup");
+
+    compuCuotasInfo.insertAdjacentElement("beforeend", div);
+
+    div.classList.add("legal");
+}
+
